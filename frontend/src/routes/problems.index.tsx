16794dd65ab85dfcb9f85b83f566de9ba7297416
plumlariat <ericjeffcoat85@gmail.com>
2026-07-26 21:10:53 -0500

    Refactored ProblemList to use react query and loaders.

--- frontend/src/routes/problems.index.tsx
+++ frontend/src/routes/problems.index.tsx
@@ -1,11 +1,64 @@
-import { createFileRoute } from '@tanstack/react-router'
-import  ProblemList from "../components/problem/ProblemList"
+import { createFileRoute } from '@tanstack/react-router';
+import { API_BASE_URL } from '../api/client';
+import type PaginatedProblems from '../types/PaginatedProblemInterface';
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import ProblemListItem from '../components/problem/ProblemListItem';
 
+const url = API_BASE_URL + "/api/problems/";
+
+const problemListQuery = (url : string) => {
+  return queryOptions({
+  queryKey: ['problemList', { url }],
+  queryFn: async (): Promise<PaginatedProblems> => {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error(`Failed to fetch problems: ${response.status}`);
+      return (await response.json()) as PaginatedProblems;
+    }
+  });
+}
 
 export const Route = createFileRoute('/problems/')({
-  component: RouteComponent,
+  loader: ({ context }) => context.queryClient.ensureQueryData(problemListQuery(url)),
+  component: ProblemList,
 })
 
-function RouteComponent() {
-  return <ProblemList />
+function ProblemList() {
+  const [currentURL, setCurrentURL] = useState<string>(url);
+  const { data } = useSuspenseQuery(problemListQuery(currentURL));
+
+  return (
+    <div>
+
+      <div>
+        Total Results: {data.count ? data.count : 0}
+      </div>
+
+      <nav>
+        <button
+          disabled={!data.previous}
+          onClick={() => data.previous && setCurrentURL(data.previous)}>
+          Previous
+        </button>
+
+        <button
+          disabled={!data.next}
+          onClick={() => data.next && setCurrentURL(data.next)}>
+          Next
+        </button>
+      </nav>
+      
+      <ul>
+        {data.results.map((problem) => (
+          <ProblemListItem 
+            key={problem.id}
+            id={problem.id}
+            problemNumber={problem.problem_no}
+            problemTitle={problem.title}
+          />
+        ))}
+      </ul>
+
+    </div>
+  );
 }
