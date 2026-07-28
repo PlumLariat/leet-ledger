3d9ebcb62ed66e06304164d2ad7284f3b6c0a602
plumlariat <ericjeffcoat85@gmail.com>
2026-07-27 23:23:00 -0500

    Updated API to allow requests for attempts by problem ID.

--- backend/progress/views.py
+++ backend/progress/views.py
@@ -3,5 +3,12 @@ from .models import Attempt
 from .serializers import AttemptSerializer
 
 class AttemptViewSet(ModelViewSet):
-    queryset = Attempt.objects.select_related('problem').prefetch_related('problem__patterns').order_by('-date')
+    queryset = Attempt.objects.all()
     serializer_class = AttemptSerializer
+
+    def get_queryset(self):
+        queryset = Attempt.objects.select_related('problem').prefetch_related('problem__patterns').order_by('-date')
+        problem = self.request.query_params.get('problem')
+        if problem is not None:
+            queryset = queryset.filter(problem=problem)
+        return queryset
